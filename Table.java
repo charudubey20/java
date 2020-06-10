280a4734a3f77cc6a618f8b2f39438b55091d6fc
charu dubey <charudubey20@gmail.com>
2020-06-11 00:07:48 +0530

    table of any number

---
+++ Table.java
@@ -0,0 +1,12 @@
+public class Table
+{
+   public static void main(String[] args)
+{
+    int num=3;
+    
+    for(int i=1;i<=10;i++)
+{
+    System.out.printf(num + "x" + i + num*i);
+        }
+    }
+}
