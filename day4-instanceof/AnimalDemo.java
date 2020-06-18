c6043af79cf4cb54fd44fdafcd98d86c59356a88
charudubey20 <61387192+charudubey20@users.noreply.github.com>
2020-06-18 15:58:11 +0530

    Add files via upload

---
+++ day4-instanceof/AnimalDemo.java
@@ -0,0 +1,72 @@
+/*create a hierarchy
+Animal
+void makeSound()
+					
+override "makeSound()" in all the child classes.
+
+Dog		Cat		Tiger
+				void hunting()
+write a class "AnimalDemo" with main and perform functions
+
+perform function should be defined in such a way that it can accept any animal and invoke "makeSound()" function.
+
+also make sure if "Tiger" is passed , its "hunting()" method also should be called.*/
+
+
+class Animal
+{
+	void makeSound()
+	{
+		
+	}
+}
+class Dog extends Animal
+{
+	void makeSound()
+	{
+		System.out.println("dog barks bow bow....!!");
+	}
+	
+}
+class Cat extends Animal
+{
+	void makeSound()
+	{
+		System.out.println("cat says meow meow....!!");
+	}
+	
+}
+class Tiger extends Animal
+{
+	void makeSound()
+	{
+		System.out.println("tiger roars...!!");
+	}
+	void hunting()
+	{
+		System.out.println("tiger mainly rely on their sense of sight and hearing for hunting rather than smell");
+	}
+	
+}
+
+public class AnimalDemo 
+{
+ static void perform(Animal ref) 
+ {
+	 if(ref instanceof Tiger)
+	 {
+		Tiger obj=(Tiger)ref;
+		obj.hunting();
+	 }
+	 ref.makeSound();
+ }
+	public static void main(String[] args)
+	{
+		perform(new Tiger());
+		perform(new Dog());
+		perform(new Cat());
+		
+
+	}
+
+}
---
+++ day4-instanceof/CourseDemo.java
@@ -0,0 +1,89 @@
+/*create a hierarchy
+Course
+void start()
+void end()
+
+override "start()" and "end()" in all the child classes.
+
+MsCit		Basic		Dbda
+				void orientation()
+write a class "CourseDemo" with main and perform functions
+
+perform function should be defined in such a way that it can accept any course and invoke "start()" and "end()" functions.
+
+also make sure if "Dbda" is passed , its "orientation()" method also should be called.*/
+
+
+
+class Course
+{
+	void start()
+	{
+		System.out.println("course start");
+	}
+	void end()
+	{
+		System.out.println("course end");
+	}
+}
+class Mscit extends Course
+{
+	void start()
+	{
+		System.out.println("mscit course start");
+	}
+	void end()
+	{
+		System.out.println("mscit course end");
+	}
+}
+class Basic extends Course
+{
+	void start()
+	{
+		System.out.println("basic course start");
+	}
+	void end()
+	{
+		System.out.println("basic course end");
+	}
+}
+class Dbda extends Course
+{
+	void start()
+	{
+		System.out.println("dbda course start");
+	}
+	void end()
+	{
+		System.out.println("dbda course end");
+	}
+	void orientation()
+	{
+		System.out.println("dbda orientation");
+	}
+}
+
+public class CourseDemo
+{
+	static void perform(Course ref)
+	{
+		if(ref instanceof Dbda)
+		{
+			Dbda obj=(Dbda)ref;
+			obj.orientation();
+		}
+		ref.start();
+		ref.end();
+	}
+
+	public static void main(String[] args)
+	{
+	    perform(new Mscit());
+	    perform(new Basic());
+	    perform(new Dbda());
+	    
+
+	}
+
+}
